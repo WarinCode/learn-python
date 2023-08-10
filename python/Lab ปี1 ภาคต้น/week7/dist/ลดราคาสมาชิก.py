b572270c0e961be7f13e183223a91891a335db35
VarinCode <warinsaipanya471@outlook.co.th>
2023-08-10 15:35:16 +0700

    การบ้านใหม่

--- python/Lab ปี1 ภาคต้น/week7/dist/ลดราคาสมาชิก.py
+++ python/Lab ปี1 ภาคต้น/week7/dist/ลดราคาสมาชิก.py
@@ -1,11 +1,11 @@
 # ข้อ 1
 Amount = float(input("จำนวนเงินที่ซื้อ >> "))
 Membership = input("เป็นสมาชิก (y/n) >> ")
-Membership = Membership == "y" or Membership == "y".upper()
+isMembership = Membership == "y" or Membership == "y".upper()
 
 def Discount(percentage , discount):
-    if Amount > 5000 and Membership: percentage = 20
-    elif Amount > 5000 and not Membership: percentage = 10
+    if Amount > 5000 and isMembership: percentage = 20
+    elif Amount > 5000 and not isMembership: percentage = 10
     else: percentage = 5    
     discount = Amount * ((100 - percentage) / 100)
     return f'คุณได้รับส่วนลด 20% = {(Amount - discount):,.2f}\nยอดเงินที่ต้องจ่ายคือ = {discount:,.2f}'
