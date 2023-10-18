6f2371dd38fbd88df27f75d5991b1ae0223dbfcb
VarinCode <warinsaipanya471@outlook.co.th>
2023-10-19 02:09:40 +0700

    เพิ่มไฟล

---
+++ 1.py
@@ -0,0 +1,4 @@
+# ข้อ 1
+n = int(input('input number : '))
+for i in range(1 , n + 1):
+    print(f'{i} ' * i)
---
+++ 2.py
@@ -0,0 +1,10 @@
+# ข้อ 2
+days = 0
+_sum = 0
+isZeroNumber = 1
+while isZeroNumber != 0:
+    days += 1
+    liters = int(input(f'Day {days}: Filling Fuel : '))
+    _sum += liters
+    isZeroNumber = liters
+print(f'Average Filling Fuel of month : {(_sum / days):,.2f}')
