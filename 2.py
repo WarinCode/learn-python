dbe1fe773f1f3dc1189ab987a47a0c31903a546d
VarinCode <warinsaipanya471@outlook.co.th>
2023-10-19 02:12:23 +0700

    updated file

--- 2.py
+++ 2.py
@@ -4,7 +4,7 @@ _sum = 0
 isZeroNumber = 1
 while isZeroNumber != 0:
     days += 1
-    liters = int(input(f'Day {days}: Filling Fuel : '))
+    liters = int(input(f'Day {days} : Filling Fuel : '))
     _sum += liters
     isZeroNumber = liters
 print(f'Average Filling Fuel of month : {(_sum / days):,.2f}')
